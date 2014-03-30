159ca1d4bf727d847c3dcedf2786da828dafe87a
mmnumbp <JasonPowersMurray@gmail.com>
2014-03-29 19:39:25 -0700

    Initial commit

---
+++ desktop/src/com/sweatyreptile/losergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sweatyreptile.losergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sweatyreptile.losergame.LoserGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LoserGame(), config);
+	}
+}
---
+++ gwt/src/com/sweatyreptile/losergame/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.sweatyreptile.losergame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.sweatyreptile.losergame.LoserGame;
+
+public class GwtLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LoserGame();
+        }
+}
