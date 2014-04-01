e0ecdf14a3d4594b199f7e1e9735c1219575ac14
mmnumbp <JasonPowersMurray@gmail.com>
2014-04-01 11:51:14 -0700

    Set up working physics and a wonky incorrect camera

--- core/src/com/sweatyreptile/losergame/TestLevelScreen.java
+++ core/src/com/sweatyreptile/losergame/TestLevelScreen.java
@@ -3,15 +3,33 @@ package com.sweatyreptile.losergame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class TestLevelScreen implements Screen {
 
+	private Camera camera;
 	private SpriteBatch spriteRenderer;
-	private Texture image;
+	private Box2DDebugRenderer physRenderer;
+	
 	private AssetManager assets;
+	private Texture image;
+	
+	private World physWorld;
 	
 	public TestLevelScreen(SpriteBatch batch, AssetManager assets){
 		spriteRenderer = batch;
@@ -23,19 +41,65 @@ public class TestLevelScreen implements Screen {
 		Gdx.gl.glClearColor(0.5f, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		physWorld.step(1/60f, 6, 2); // TODO: Change step
+		
+		physRenderer.render(physWorld, camera.combined);
+		
 		spriteRenderer.begin();
-		spriteRenderer.draw(image, 0, 0);
+		//spriteRenderer.draw(image, 0, 0);
 		spriteRenderer.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-
+		camera.position.set(new Vector3(width / 2, height / 2, 0));
+		Gdx.app.log("wow", "height: " + height + ", width: " + width);
+		camera.viewportHeight = 144;
+		camera.viewportWidth = 256;
+		spriteRenderer.setProjectionMatrix(camera.combined);
 	}
 
 	@Override
 	public void show() {
+		int width = Gdx.graphics.getWidth();
+		int height = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(width, height);
+		resize(width, height);
 		image = assets.get("badlogic.jpg", Texture.class);
+		physWorld = new World(new Vector2(0, -9.8f), true);
+		physRenderer = new Box2DDebugRenderer();
+		setupWorld();
+	}
+	
+	private void setupWorld() {
+		BodyDef bodyDef = new BodyDef();
+		BodyDef groundDef = new BodyDef();
+		
+		bodyDef.type = BodyType.DynamicBody;
+		groundDef.type = BodyType.StaticBody;
+		
+		bodyDef.position.set(0, 100f);
+		groundDef.position.set(0, 0);
+		
+		Body body = physWorld.createBody(bodyDef);
+		Body groundBody = physWorld.createBody(groundDef);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		CircleShape circle = new CircleShape();
+		circle.setRadius(20f);
+		fixtureDef.shape = circle;
+		fixtureDef.density = 2f;
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 2f;
+		Fixture fixture = body.createFixture(fixtureDef);
+		
+		PolygonShape groundBox = new PolygonShape();
+		groundBox.setAsBox(camera.viewportWidth, 10f);
+		groundBody.createFixture(groundBox, 0f);
+		
+		circle.dispose();
+		groundBox.dispose();
+		
 	}
 
 	@Override
