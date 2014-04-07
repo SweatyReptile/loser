b54ea5b4cb0a4d34256f3b9df494964220123491
mmnumbp <JasonPowersMurray@gmail.com>
2014-04-07 10:51:40 -0700

    Clean imports

--- core/src/com/sweatyreptile/losergame/TestLevelScreen.java
+++ core/src/com/sweatyreptile/losergame/TestLevelScreen.java
@@ -15,7 +15,6 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
