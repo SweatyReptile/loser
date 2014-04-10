03ccc25d9b252f35310ba3dbe2f34b54d800db51
mmnumbp <JasonPowersMurray@gmail.com>
2014-04-10 11:33:58 -0700

    Create AssetManagerPlus, which can handle the loading of PhysicsBody EditorLoaders

---
+++ core/src/com/sweatyreptile/losergame/loaders/AssetManagerPlus.java
@@ -0,0 +1,71 @@
+package com.sweatyreptile.losergame.loaders;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import aurelienribon.bodyeditor.FixedBodyEditorLoader;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.FileHandleResolver;
+
+/**
+ * 
+ * Includes physics body editor loader
+ *
+ */
+public class AssetManagerPlus extends AssetManager {
+
+	private List<String> loaderPaths;
+	private Map<String, FixedBodyEditorLoader> bodyEditorLoaders;
+	private boolean bodiesLoaded;
+	
+	
+	public AssetManagerPlus() {
+		super();
+		loaderPaths = new ArrayList<String>();
+		bodyEditorLoaders = new HashMap<String, FixedBodyEditorLoader>();
+	}
+
+	public AssetManagerPlus(FileHandleResolver resolver) {
+		super(resolver);
+		loaderPaths = new ArrayList<String>();
+		bodyEditorLoaders = new HashMap<String, FixedBodyEditorLoader>();
+	}
+
+	@Override
+	public synchronized <T> void load(String fileName, Class<T> type) {
+		if (type.equals(FixedBodyEditorLoader.class)) {
+			loaderPaths.add(fileName);
+		}
+		else{
+			super.load(fileName, type);
+		}
+	}
+
+	@Override
+	public synchronized boolean update() {
+		if (!bodiesLoaded) {
+			for (String loaderNameString : loaderPaths) {
+				bodyEditorLoaders.put(loaderNameString,
+						new FixedBodyEditorLoader(Gdx.files.internal(loaderNameString)));
+			}
+			bodiesLoaded = true;
+		}
+		return super.update();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public synchronized <T> T get(String fileName, Class<T> type) {
+		if (type.equals(FixedBodyEditorLoader.class)) {
+			return (T) bodyEditorLoaders.get(fileName);
+		}
+		return super.get(fileName, type);
+	}
+	
+	
+	
+}
