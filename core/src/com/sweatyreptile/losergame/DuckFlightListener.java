598092fa095281012d2b977ac212b6d67888a120
Sharbal <ok_deal@ymail.com>
2014-04-18 22:04:58 -0700

    Store contacts in duckFlightListener

--- core/src/com/sweatyreptile/losergame/DuckFlightListener.java
+++ core/src/com/sweatyreptile/losergame/DuckFlightListener.java
@@ -3,20 +3,36 @@ package com.sweatyreptile.losergame;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class DuckFlightListener implements ContactListener {
 
+	private int contacts;
+	
 	@Override
-	public void beginContact(Contact contact) {
-		// TODO Auto-generated method stub
-
+	public void beginContact(Contact contact) {	
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+		if ((fixtureA != null && fixtureA.getBody() != null && fixtureA.getBody().getUserData() != null &&
+				fixtureA.getBody().getUserData().equals("flight_sensor"))
+			|| (fixtureB != null && fixtureB.getBody() != null && fixtureB.getBody().getUserData() != null &&
+				fixtureB.getBody().getUserData().equals("flight_sensor"))){
+			contacts++;
+		}
+		
 	}
 
 	@Override
 	public void endContact(Contact contact) {
-		// TODO Auto-generated method stub
-
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+		if ((fixtureA != null && fixtureA.getBody() != null && fixtureA.getBody().getUserData() != null &&
+				fixtureA.getBody().getUserData().equals("flight_sensor"))
+			|| (fixtureB != null && fixtureB.getBody() != null && fixtureB.getBody().getUserData() != null &&
+				fixtureB.getBody().getUserData().equals("flight_sensor"))){
+			contacts--;
+		}
 	}
 
 	@Override
@@ -29,6 +45,11 @@ public class DuckFlightListener implements ContactListener {
 	public void postSolve(Contact contact, ContactImpulse impulse) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public boolean isFlying(){
+		if (contacts == 0) return true;
+		return false;
 	}
 
 }
