8683d48f89f3c36058bbf690b8f9a3b774e494ca
Jason Murray <JasonPowersMurray@gmail.com>
2014-04-18 19:53:14 -0700

    Temporary fix to load native library before loading bodies (workaround for https://github.com/libgdx/libgdx/issues/1730)

--- core/src/com/sweatyreptile/losergame/screens/LoadingScreen.java
+++ core/src/com/sweatyreptile/losergame/screens/LoadingScreen.java
@@ -2,6 +2,8 @@ package com.sweatyreptile.losergame.screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class LoadingScreen extends FinishableScreen{
 
@@ -9,6 +11,7 @@ public class LoadingScreen extends FinishableScreen{
 	
 	public LoadingScreen(ScreenFinishedListener listener, AssetManager loader, Screen nextScreen) {
 		super(listener, nextScreen);
+		new World(new Vector2(0, 0), false);
 		this.loader = loader;
 	}
 
