b40dfa272fbd49536f05f77b1e5e0f1197275b1b
Jason Murray <JasonPowersMurray@gmail.com>
2014-04-27 15:25:44 -0700

    Remove unused code

--- core/src/com/sweatyreptile/losergame/screens/TestLevelScreen.java
+++ core/src/com/sweatyreptile/losergame/screens/TestLevelScreen.java
@@ -146,7 +146,6 @@ public class TestLevelScreen implements Screen {
 		ef.create("book_yellow", BodyType.DynamicBody, 1.2f, 1.1f, new WoodFixtureDef(assets, "book_yellow"), false);
 		ef.create("shelf", BodyType.StaticBody, 1f, 1f, new EntityFixtureDef(assets, "shelf"), false);
 		ef.create("pencil", BodyType.DynamicBody, 1.6f, 0.7f, new WoodFixtureDef(assets, "pencil"), false);
-		//ef.create("radio", BodyType.DynamicBody, 1.4f, 1.1f, new MetalFixtureDef(assets, "radio"), false);
 		
 		BodyDef radioBodyDef = new BodyDef();
 		radioBodyDef.type = BodyType.DynamicBody;
