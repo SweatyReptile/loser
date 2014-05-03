0516f66a61afa0869a46cec758dfcf6b8edeeef6
Jason Murray <JasonPowersMurray@gmail.com>
2014-05-03 10:57:19 -0700

    Set up world just like in TestLevelScreen, for test purposes

--- core/src/com/sweatyreptile/losergame/screens/ScrollingLevelScreen.java
+++ core/src/com/sweatyreptile/losergame/screens/ScrollingLevelScreen.java
@@ -2,9 +2,19 @@ package com.sweatyreptile.losergame.screens;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.sweatyreptile.losergame.Entity;
+import com.sweatyreptile.losergame.EntityFactory;
 import com.sweatyreptile.losergame.Player;
 import com.sweatyreptile.losergame.PlayerInputProcessor;
+import com.sweatyreptile.losergame.entities.MusicPlayer;
+import com.sweatyreptile.losergame.fixtures.DuckFixtureDef;
+import com.sweatyreptile.losergame.fixtures.EntityFixtureDef;
+import com.sweatyreptile.losergame.fixtures.MetalFixtureDef;
+import com.sweatyreptile.losergame.fixtures.WoodFixtureDef;
 import com.sweatyreptile.losergame.loaders.AssetManagerPlus;
 
 public class ScrollingLevelScreen extends LevelScreen {
@@ -16,6 +26,11 @@ public class ScrollingLevelScreen extends LevelScreen {
 				viewportWidth, viewportHeight);
 		
 	}
+	
+	@Override
+	public void update(float delta) {
+		super.update(delta);
+	}
 
 	@Override
 	protected Player createPlayer() {
@@ -25,9 +40,27 @@ public class ScrollingLevelScreen extends LevelScreen {
 		return new Player(world, contactListener, def, assets);
 	}
 
-	@Override
 	protected void setupWorld() {
+		EntityFactory ef = entityFactory;
+		
+		ef.create("dead_duck", BodyType.DynamicBody, 1.4f, .5f, new DuckFixtureDef(assets), false);
+		ef.create("wash_machine", BodyType.StaticBody, .5f, .1f, new EntityFixtureDef(assets, "wash_machine"), false);
+		ef.create("cereal", BodyType.DynamicBody, 1.8f, 0.7f, new EntityFixtureDef(assets, "cereal"), false);
+		ef.create("table", BodyType.StaticBody, 1.25f, .1f, new EntityFixtureDef(assets, "table"), false);
+		ef.create("book_blue", BodyType.DynamicBody, 1.1f, 1.1f, new WoodFixtureDef(assets, "book_blue"), false);
+		ef.create("book_red", BodyType.DynamicBody, 1.15f, 1.1f, new WoodFixtureDef(assets, "book_red"), false);
+		ef.create("book_yellow", BodyType.DynamicBody, 1.2f, 1.1f, new WoodFixtureDef(assets, "book_yellow"), false);
+		ef.create("shelf", BodyType.StaticBody, 1f, 1f, new EntityFixtureDef(assets, "shelf"), false);
+		ef.create("pencil", BodyType.DynamicBody, 1.6f, 0.7f, new WoodFixtureDef(assets, "pencil"), false);
+		
+		BodyDef groundDef = new BodyDef();
+		groundDef.type = BodyType.StaticBody;
+		groundDef.position.set(viewportWidth / 2, 0);
+		PolygonShape groundBox = new PolygonShape();
+		groundBox.setAsBox(camera.viewportWidth / 2, .1f);
+		Body groundBody = world.createBody(groundDef);
+		groundBody.createFixture(groundBox, 0f);
+		groundBox.dispose();
 		
 	}
-
 }
