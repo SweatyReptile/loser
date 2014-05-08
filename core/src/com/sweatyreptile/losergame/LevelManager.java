58e8885da1b4878568a4830f6217dd2a715b3474
Jason Murray <JasonPowersMurray@gmail.com>
2014-05-08 16:46:24 -0700

    Instantiating a LevelScreen also instantiates the associated TitleScreen

--- core/src/com/sweatyreptile/losergame/LevelManager.java
+++ core/src/com/sweatyreptile/losergame/LevelManager.java
@@ -3,16 +3,18 @@ package com.sweatyreptile.losergame;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sweatyreptile.losergame.loaders.AssetManagerPlus;
 import com.sweatyreptile.losergame.screens.LevelScreen;
+import com.sweatyreptile.losergame.screens.LevelTitleScreen;
 import com.sweatyreptile.losergame.screens.ScreenFinishedListener;
 import com.sweatyreptile.losergame.screens.TestScrollingLevelScreen;
 
 public class LevelManager {
 
+	private Map<String, LevelTitleScreen> levelTitles;
 	private Map<String, LevelScreen> levels;
-	private LevelScreen currentLevel;
 	private SpriteBatch batch;
 	private AssetManagerPlus assets;
 	private PlayerInputProcessor inputProcessor;
@@ -31,24 +33,27 @@ public class LevelManager {
 		this.screenHeight = screenHeight;
 		
 		levels = new HashMap<String, LevelScreen>();
+		levelTitles = new HashMap<String, LevelTitleScreen>();
 		
-		instantiateScreen("testHome", "TestScrollingLevelScreen", "WELCOME TO THE JUNGLE", 3.2f, 1.8f, 30f);
+		instantiate("test_home", "TestScrollingLevelScreen", "WELCOME TO THE JUNGLE", 3.2f, 1.8f, 30f);
 	}
 	
-	public void instantiateScreen(String alias, String typeName, String levelName,
+	public void instantiate(String alias, String typeName, String levelName,
 			float viewportWidth, float viewportHeight, float timeLimit){
+		
+		LevelTitleScreen titleScreen = new LevelTitleScreen(
+				batch, assets, screenWidth, screenHeight, alias, levelName);
 		
 		LevelScreen level = LevelScreen.newInstance(
 				typeName, this, batch, assets,
 				inputProcessor, screenWidth, screenHeight,
-				viewportWidth, viewportHeight, timeLimit, levelName);
+				viewportWidth, viewportHeight, timeLimit, alias, levelName);
 		
 		levels.put(alias, level);
+		levelTitles.put(alias, titleScreen);
 		
+		Gdx.app.log("LevelManager", "Instantiated " + alias);
 	}
 	
-	public void update(float delta){
-		
-	}
-
+	
 }
