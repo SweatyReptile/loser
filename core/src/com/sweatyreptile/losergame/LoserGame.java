4ac9dd5c1f8918aa1044fb5fd8ee01a7a75700f9
Jason Murray <JasonPowersMurray@gmail.com>
2014-05-16 19:44:14 -0700

    Wire up FreeTypeFontLoader with AssetManagerPlus and load a font

--- core/src/com/sweatyreptile/losergame/LoserGame.java
+++ core/src/com/sweatyreptile/losergame/LoserGame.java
@@ -6,11 +6,15 @@ import aurelienribon.tweenengine.Tween;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.sweatyreptile.losergame.loaders.AssetManagerPlus;
+import com.sweatyreptile.losergame.loaders.FreeTypeFontLoader;
+import com.sweatyreptile.losergame.loaders.FreeTypeFontParameters;
 import com.sweatyreptile.losergame.screens.FinishableScreen;
 import com.sweatyreptile.losergame.screens.LevelScreen;
 import com.sweatyreptile.losergame.screens.LoadingScreen;
@@ -27,10 +31,18 @@ public class LoserGame extends Game implements ScreenFinishedListener{
 		Tween.registerAccessor(LevelScreen.class, new ScrollingLevelAccessor());
 		
 		assets = new AssetManagerPlus();
+		assets.setLoader(BitmapFont.class, new FreeTypeFontLoader(new InternalFileHandleResolver()));
+		
 		batch = new SpriteBatch();
 		int screenWidth = Gdx.graphics.getWidth();
 		int screenHeight = Gdx.graphics.getHeight();
 
+		
+		FreeTypeFontParameters speechParameter = new FreeTypeFontParameters();
+		speechParameter.parameters.size = 18;
+		
+		assets.load("corbelb.ttf", BitmapFont.class, speechParameter);
+		
 		assets.load("badlogic.jpg", Texture.class);
 		assets.load("duck.json", FixedBodyEditorLoader.class);
 		
