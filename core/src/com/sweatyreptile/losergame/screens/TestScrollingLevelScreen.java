8192b5b9d9a8961e3c09eda5a0fce21402df74ab
Sharbal <ok_deal@ymail.com>
2014-05-18 15:11:59 -0700

    Create LightFixtureDef and alter positions of entities in test screen
    LightFixtureDef used for cereal only so far
    Gives entities a density of 5f

--- core/src/com/sweatyreptile/losergame/screens/TestScrollingLevelScreen.java
+++ core/src/com/sweatyreptile/losergame/screens/TestScrollingLevelScreen.java
@@ -11,6 +11,7 @@ import com.sweatyreptile.losergame.entities.Player;
 import com.sweatyreptile.losergame.entities.Sharbal;
 import com.sweatyreptile.losergame.fixtures.DuckFixtureDef;
 import com.sweatyreptile.losergame.fixtures.EntityFixtureDef;
+import com.sweatyreptile.losergame.fixtures.LightFixtureDef;
 import com.sweatyreptile.losergame.fixtures.MetalFixtureDef;
 import com.sweatyreptile.losergame.fixtures.WoodFixtureDef;
 
@@ -44,15 +45,15 @@ public class TestScrollingLevelScreen extends ScrollingLevelScreen {
 		
 		EntityFactory ef = entityFactory;
 		
-		ef.create("dead_duck", BodyType.DynamicBody, 1.4f, .5f, new DuckFixtureDef(assets), false);
+		ef.create("dead_duck", BodyType.DynamicBody, 1.3f, .5f, new DuckFixtureDef(assets), false);
 		ef.create("wash_machine", BodyType.StaticBody, .5f, .1f, new EntityFixtureDef(assets, "wash_machine"), false);
-		ef.create("cereal", BodyType.DynamicBody, 1.8f, 0.7f, new EntityFixtureDef(assets, "cereal"), false);
-		ef.create("table", BodyType.StaticBody, 1.25f, .1f, new EntityFixtureDef(assets, "table"), false);
+		ef.create("cereal", BodyType.DynamicBody, 3.05f, 0.7f, new LightFixtureDef(assets, "cereal"), false);
+		ef.create("table", BodyType.StaticBody, 2.5f, .1f, new EntityFixtureDef(assets, "table"), false);
 		ef.create("book_blue", BodyType.DynamicBody, 1.1f, 1.1f, new WoodFixtureDef(assets, "book_blue"), false);
 		ef.create("book_red", BodyType.DynamicBody, 1.15f, 1.1f, new WoodFixtureDef(assets, "book_red"), false);
 		ef.create("book_yellow", BodyType.DynamicBody, 1.2f, 1.1f, new WoodFixtureDef(assets, "book_yellow"), false);
 		ef.create("shelf", BodyType.StaticBody, 1f, 1f, new EntityFixtureDef(assets, "shelf"), false);
-		ef.create("pencil", BodyType.DynamicBody, 1.6f, 0.7f, new WoodFixtureDef(assets, "pencil"), false);
+		ef.create("pencil", BodyType.DynamicBody, 2.85f, 0.7f, new WoodFixtureDef(assets, "pencil"), false);
 		ef.create("wall_left", BodyType.StaticBody, -BORDER_WIDTH, 0f, new EntityFixtureDef(assets, "vertical_border"), false);
 		ef.create("wall_right", BodyType.StaticBody, bgWidth, 0f, new EntityFixtureDef(assets, "vertical_border"), false);
 		
@@ -66,7 +67,7 @@ public class TestScrollingLevelScreen extends ScrollingLevelScreen {
 		
 		BodyDef sharbalBodyDef = new BodyDef();
 		sharbalBodyDef.type = BodyType.StaticBody;
-		sharbalBodyDef.position.set(2.7f, 0.1f);
+		sharbalBodyDef.position.set(4.5f, 0.1f);
 		sharbal = new Sharbal(world, contactListener, sharbalBodyDef, assets,
 				new EntityFixtureDef(assets, "sharbal_test"), false, 
 				Entity.DEFAULT_SCREEN_WIDTH, viewportWidth, player);
