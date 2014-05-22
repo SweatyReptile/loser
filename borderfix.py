5987316e2204fd150aefd626281997cea4fecce7
mnopbop <JasonPowersMurray@gmail.com>
2014-05-22 11:47:42 -0700

    Single file border generation with variable size

--- borderfix.py
+++ borderfix.py
@@ -1,7 +1,7 @@
 """
-Usage: 
+Usage:
     borderfix (file|folder) [options] <name>...
-    
+
 Options:
     -s BORDER_SIZE_PIXELS, --size BORDER_SIZE_PIXELS
 """
@@ -14,27 +14,31 @@ def process_image(filepath, size):
         image = Image.open(filepath)
     except:
         print("Unable to find file: ", filepath)
-    
-    size = size-1
-    newsize = (image.size[0] + size*2, image.size[1] + size*2)
-    newimage = Image.new(image.mode, image.size)
 
-    for x in xrange(0, newimage.size[0]):
-        for y in xrange(0, newimage.size[1]):
-            if (x > size and y > size):
-                imgxy = (x-size, y-size)
-                newxy = (x, y)
-                pixel = image.getpixel(imgxy)
-                newimage.putpixel(newxy, pixel)
-    newimage.show()
-    
+    newsize = (image.size[0] + size*2, image.size[1] + size*2)
+    newimage = Image.new("RGBA", newsize, (0, 0, 0, 0))
+
+    for x in range(0, image.size[0]):
+        for y in range(0, image.size[1]):
+            imgxy = (x, y)
+            newxy = (x+size, y+size)
+            pixel = image.getpixel(imgxy)
+            newimage.putpixel(newxy, pixel)
+    newimage.save(filepath + ".1", "png")
+
+def getsize(args):
+    size = args.get("--size")
+    if size == None:
+        size = 1
+    return int(size)
+
 def process(args):
     if args.get("file"):
         for name in args.get("<name>"):
-            process_image(name, int(args.get("--size")))
+            process_image(name, getsize(args))
     elif args.get("folder"):
         print("folder")
-        
+
 if __name__ == "__main__":
     args = docopt(__doc__)
     print(args)
