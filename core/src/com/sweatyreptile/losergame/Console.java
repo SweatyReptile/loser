4cd7495eceb22f589466df81682112ea700255b1
Jason Murray <JasonPowersMurray@gmail.com>
2014-06-19 19:53:45 -0700

    Create Console class
    Right now it's barebones..
    
    It temporarily has its own SpriteBatch because otherwise it would be
    drawn too big when the normal SpriteBatch has its projection matrix
    changed by LevelScreen.
    
    Possible fixes to this include either:
    -use 2 SpriteBatches, one for UI and one for everything else
    -use 1 SpriteBatch, and switch between projection matrices to draw UI
    
    I'm not sure which one is more performant.

---
+++ core/src/com/sweatyreptile/losergame/Console.java
@@ -0,0 +1,45 @@
+package com.sweatyreptile.losergame;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Console {
+
+	private SpriteBatch spriteRenderer;
+	private AssetManager assets;
+	private Sprite bg;
+	private Sprite textbg;
+	private int height;
+	private int width;
+	private int textHeight;
+	
+	public Console(SpriteBatch spriteRenderer, AssetManager assets, int width, int height) {
+		this.spriteRenderer = new SpriteBatch();
+		this.assets = assets;
+		this.height = height;
+		this.width = width;
+		textHeight = 20;
+		
+	}
+	
+	public void init(){
+		bg = new Sprite(assets.get("img/ui/console_bg.png", Texture.class));
+		textbg = new Sprite(assets.get("img/ui/console_textfield.png", Texture.class));
+		
+		bg.setSize(width, height);
+		textbg.setSize(width, textHeight);
+	}
+	
+	public void render(){
+		if (bg != null && textbg != null){
+			spriteRenderer.begin();
+			bg.draw(spriteRenderer);
+			textbg.draw(spriteRenderer);
+			spriteRenderer.end();
+		}
+	}
+	
+
+}
